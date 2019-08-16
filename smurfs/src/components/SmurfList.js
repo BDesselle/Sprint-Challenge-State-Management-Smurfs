7ad99b16235f92e4764d85e88605bd5ef8c2ee32
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-16 18:35:17 -0500

    Update SmurfList.js

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,30 +1,40 @@
-import React, { useEffect } from "react";
+import React from "react";
+//* Importing Connect function
 import { connect } from "react-redux";
+//* Importing the getSmurfs Action
 import { getSmurfs } from "../store/actions";
+// Components
 import Smurf from "./Smurf";
 
 const SmurfList = props => {
-  useEffect(() => {
-    props.getSmurfs(props.smurfs);
+  //* Creating a useEffect _ componentDidMount || componentDidUpdate _ dependency=props.smurfs.name
+  React.useEffect(() => {
+    props.getSmurfs(props.smurfs); //* onMount || onUpdate invoke the function to dispatch an action from the store (actions.js)
     // eslint-disable-next-line
   }, [props.smurfs.name]);
 
+  //* if !props.smurfs = true, return "Loading..."
   if (!props.smurfs) return <p>Loading...</p>;
+  //* else return SmurfList JSX
   return (
-    <div>
-      {props.smurfs.map((smurf, index) => (
-        <Smurf smurf={smurf} key={index} />
-      ))}
-    </div>
+    <React.Fragment>
+      <div>
+        {props.smurfs.map((smurf, index) => (
+          <Smurf smurf={smurf} key={index} />
+        ))}
+      </div>
+    </React.Fragment>
   );
 };
 
+//* Mapping my State to Props
 const mapStatetoProps = state => {
   return {
     smurfs: state.smurfs
   };
 };
 
+//* Using the Connect function to connect my component to the Redux Store & provides the function it can use to dispatch actions from the store
 export default connect(
   mapStatetoProps,
   { getSmurfs }
