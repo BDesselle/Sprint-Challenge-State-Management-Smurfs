1697ce7c1e9827350002ded31567ff4c5c34a746
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-16 18:35:26 -0500

    Update index.js

--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -1,3 +1,4 @@
+//* Importing my Action Types
 import {
   GET_SMURFS_START,
   GET_SMURFS_SUCCESS,
@@ -7,6 +8,7 @@ import {
   ADD_SMURF_FAIL
 } from "../actions";
 
+//* Defining my Initial State
 const initialState = {
   smurfs: [],
   isGetting: false,
@@ -18,63 +20,73 @@ const initialState = {
   }
 };
 
+//* Creating my reducer - takes in state which is assigned to initialState, and action
 export const reducer = (state = initialState, action) => {
+  //* Using a switch statement to select which block of code gets executed via action type
   switch (action.type) {
+    //* GET_SMURFS_START
     case GET_SMURFS_START:
       console.log(
         "%cReducer > getSmurfs_STARTING...",
         "color: orange; font-weight: bold;"
       );
+      console.log("%cThis is blue", "color: blue;");
       return {
-        ...state,
-        isGetting: true
+        ...state, //* Spread State
+        isGetting: true //* Toggle isGetting
       };
+    //* GET_SMURFS_SUCCESS
     case GET_SMURFS_SUCCESS:
       console.log(
         "%cReducer > getSmurfs_SUCCESSFUL!",
         "color: green; font-weight: bold;"
       );
       return {
-        smurfs: action.payload,
-        isGetting: false
+        smurfs: action.payload, //* Change State
+        isGetting: false //* Toggle isGetting
       };
+    //* GET_SMURFS_FAIL
     case GET_SMURFS_FAIL:
       console.log(
         "%cReducer > getSmurfs_FAILURE!",
         "color: red; font-weight: bold;"
       );
       return {
-        ...state,
-        isGetting: false
+        ...state, //* Spread State
+        isGetting: false //* Toggle isGetting
       };
+    //* ADD_SMURF_START
     case ADD_SMURF_START:
       console.log(
         "%cReducer > addSmurf_STARTING...",
         "color: orange; font-weight: bold;"
       );
       return {
-        ...state,
-        isPosting: true
+        ...state, //* Spread State
+        isPosting: true //* Toggle isPosting
       };
+    //* ADD_SMURF_SUCCESS
     case ADD_SMURF_SUCCESS:
       console.log(
         "%cReducer > addSmurf_SUCCESSFUL!",
         "color: green; font-weight: bold;"
       );
       return {
-        smurfs: [...state.smurfs, action.payload],
-        isPosting: false
+        smurfs: [...state.smurfs, action.payload], //* Change State
+        isPosting: false //* Toggle isPosting
       };
+    //* ADD_SMURF_FAIL
     case ADD_SMURF_FAIL:
       console.log(
         "%cReducer > addSmurf_FAILURE!",
         "color: red; font-weight: bold;"
       );
       return {
-        ...state,
-        isPosting: false
+        ...state, //* Spread State
+        isPosting: false //* Toggle isPosting
       };
+    //* Default
     default:
-      return state;
+      return state; //* Return State
   }
 };
